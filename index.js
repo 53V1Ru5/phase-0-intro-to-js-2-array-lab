648d834617c3ecc7613a507b759bb79180443da9
Ivan <ikladinov@gmail.com>
2022-12-14 13:52:41 -0500

    commit tree c4ed95ed815a1a5f713ee8bbcb044d9aba4d1bf6

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function  destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCat = [...cats, name];
+ return newCat;
+ //   let  newCat = [...cats];
+  //newCat.unshift(name);
+    
+}
+
+function prependCat(name){
+    const newCat = [name,...cats];
+    return newCat;
+}
+
+function removeLastCat(){
+    const newCat = [...cats];
+    newCat.pop();
+    return newCat;
+}
+
+function removeFirstCat(){
+    const newCat = [...cats];
+    newCat.shift();
+    return newCat;
+}
